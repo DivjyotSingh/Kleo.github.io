45cae68c2b2a94025c600ef28b4cb6b22d88151d
heysarthak <sarthakbabbar12@gmail.com>
2022-09-29 17:10:17 +0530

    mere kaam hogya

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Button.css'
+
+function Button() {
+  return (
+    <div className='Container-1'>
+        <div className='Button-1'>
+            Menu
+        </div>
+        <div className='Button-2'>
+            Drink
+        </div>
+
+    </div>
+  )
+}
+
+export default Button
---
+++ src/ButtonPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ButtonPage.css'
+import Buttons from './Button'
+function ButtonPage() {
+  return (
+    <div className="Button_background">
+        <div>KLEO</div>
+        <Buttons/>
+    </div>
+  )
+}
+
+export default ButtonPage
