0946e0896a827f53460901ca69b776dca7108dcd
heysarthak <sarthakbabbar12@gmail.com>
2022-10-04 14:54:42 +0530

    Done

--- src/Button.js
+++ src/Button.js
@@ -5,11 +5,11 @@ function Button() {
   return (
     <div className='Container-1'>
         <div className='Button-1'>
-            <div className='Button-3'>
+            <div className='Button-3' onClick={()=>window.open("https://drive.google.com/file/d/1YSeOTvbXtnjDw_qQVrvqFztKlLR1z3R8/view?usp=sharing","_blank")}>
               Food Menu
             </div>
         </div>
-        <div className='Button-2'>
+        <div className='Button-2' onClick={()=>window.open("https://drive.google.com/file/d/1Na_RCHO2NWV-ZXirVFRJF1cQOioUxCn9/view?usp=sharing","_blank")}>
            <div className='Button-3'>
             Drink Menu
            </div> 
